d0b19e2d7a099048e27e39d18d94619b63fd460b
”ssyunya2002” <s.suzuki.spurs@gmail.com>
2019-05-23 15:10:57 +0900

    dataの表示

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      todos: [
+        {id: 1, text:'dataを表示する'},
+        {id: 2, text: '簡単な構成を知る'}
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          {this.state.todos.map((item) =>{
+            return( <li>{item.text}</li> )
+          })}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
